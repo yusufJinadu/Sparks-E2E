28e10f9f66b1b56e66035bb5de7af0b11c3ea6fa
yusufJinadu <39502587+yusufJinadu@users.noreply.github.com>
2023-01-03 12:30:23 +0100

    Update funny_cat_memes.spec.js

--- cypress/e2e/funny-cat-memes/funny_cat_memes.spec.js
+++ cypress/e2e/funny-cat-memes/funny_cat_memes.spec.js
@@ -5,7 +5,7 @@ describe(
 	'User should be able to take a screen shot of funny cat memes ',
 	{ tags: ['admin-tool'] },
 	function () {
-		this.beforeEach(() => {
+		beforeEach(() => {
 			// not really neccesary here because the texts are only used in one place, but
 			// generally I refrain from using texts in test so that they can be updated easily if they change
 			cy.fixture(fixture).then(function (data){
